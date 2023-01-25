ddb6c80ed6511d453a0b072f5d40551f36910e81
Gisele Abzalov <gisele.abzalov@gmail.com>
2023-01-25 11:40:20 -0500

    added timestamp

--- src/restaurants.js
+++ src/restaurants.js
@@ -35,7 +35,7 @@ export async function createRestaurant(req, res) {
 // Update
 export async function updateRestaurantById(req, res) {
   const { restId } = req.params
-  const updateInfo = req.body
+  let updateInfo = req.body
   // add updatedAt date here:
   const db = dbConnect()
   await db.collection(collectionName).doc(restId).update(updateInfo)
